0e23881365fd168c1ff703ee8d27bc828c62b9ed
Peter Seres <peet.seres@gmail.com>
2020-03-26 22:30:44 +0100

    Added no file selection

--- convert/__main__.py
+++ convert/__main__.py
@@ -4,6 +4,11 @@ import easygui
 
 def main():
     file_path = easygui.fileopenbox("Choose .ulg files to convert.", "AVY", filetypes="*.txt", multiple=True)
+
+    if file_path is None:
+        print(f" \n >> No files selected. Exiting program.")
+        return 1
+        
     for file in file_path:
         ulog2kml.convert_ulog2kml(ulog_file_name=file, output_file_name=file[:-4]+'.kml')
     return 0
